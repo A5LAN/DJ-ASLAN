3af8b73e8c7c6c28fc177f7984ee5a2473c26dba
A5LAN <djokanaslan@gmail.com>
2025-04-28 00:50:20 -0400

    Final Push

--- dj-aslan-app/vite.config.js
+++ dj-aslan-app/vite.config.js
@@ -6,9 +6,14 @@ export default defineConfig({
   plugins: [react()],
   base: '/DJ-ASLAN/',  // Adjust base path for deployment on GitHub Pages
   build: {
-    outDir: 'dist', // Ensure the output folder for build is 'dist' (default)
+    outDir: 'dist',  // Ensure the output folder for the build is 'dist' (default)
+    emptyOutDir: true,  // Clean the dist folder before building
+    rollupOptions: {
+      input: './public/index.html',  // Explicitly set the entry HTML file
+    },
   },
+  publicDir: 'public',  // Ensure Vite uses the 'public' folder for static assets
   server: {
-    open: true, // Automatically open the browser when the server starts
+    open: true,  // Automatically open the browser when the server starts
   },
 });
