c3ce7fcf5dd0c6fcdf0b00c150ab7b0ee08ebf75
A5LAN <djokanaslan@gmail.com>
2025-05-09 19:22:20 -0400

    updated project section

--- dj-aslan-app/src/components/ProjectsSection.jsx
+++ dj-aslan-app/src/components/ProjectsSection.jsx
@@ -6,33 +6,38 @@ const projects = [
     title: 'Weddings',
     description: 'Capturing timeless moments at weddings.',
     video: '/DJ-ASLAN/videos/wedding.mp4',
+    link: '/weddings-gallery', // Dynamic link for weddings
   },
   {
     title: 'Restaurants',
     description: 'Setting the vibe at top dining locations.',
-    image: '/DJ-ASLAN/images/restaurant.jpg',
+    image: '/DJ-ASLAN/images/restaurant.JPG',
+    link: '/restaurants-gallery', // Dynamic link for restaurants
   },
   {
     title: 'Special Events',
     description: 'Making special moments unforgettable.',
-    image: '/DJ-ASLAN/images/special-event.jpg',
+    image: '/DJ-ASLAN/images/special-event.JPG',
+    link: '/special-events-gallery', // Dynamic link for special events
   },
   {
     title: 'Night Clubs',
     description: 'Spinning at vibrant nightlife venues.',
-    image: '/DJ-ASLAN/images/project1.jpg',
+    image: '/DJ-ASLAN/images/night-club.jpg',
+    link: '/night-clubs-gallery', // Dynamic link for night clubs
   },
   {
     title: 'Concerts / Raves',
     description: 'Bringing energy to massive crowds.',
     image: '/DJ-ASLAN/images/project1.jpg',
+    link: '/concerts-raves-gallery', // Dynamic link for concerts/raves
   },
 ];
 
 const ProjectsSection = () => {
   return (
     <section id="projects" className="section">
-      <h2>Featured Projects</h2>
+      <h2>Event Gallery</h2>
       <div className="projects">
         {projects.map((project, index) => (
           <div className="project" key={index}>
@@ -59,7 +64,7 @@ const ProjectsSection = () => {
             <div className="project-info">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
-              <a href="/projects" className="view-more">
+              <a href={project.link} className="view-more">
                 View More
               </a>
             </div>
